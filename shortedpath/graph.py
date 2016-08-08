c288cf2c85a717059f0f19e3f3f3b138059dc9d5
rocky <zhengwu314@163.com>
2016-08-08 08:20:30 +0800

    shortedpath

---
+++ shortedpath/graph.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+#-*-coding=utf-8-*-
+
+import warnings
+import sys
+
+
+class Vertex(object):
+    """
+
+    vertex
+    param:
+    vertex_id : id
+    """
+
+    def __init__(self, vertex_id):
+        self.id =  vertex_id
+        self.connections=  {}
+        self.distance = sys.maxsize
+        self.pre = None
+        self.visited = False
+
+    def __str__(self):
+        return 'vertex_'+str(self.id)
+
+
+    def get_weight(self, vertex):
+        return self.connections.get(vertex, sys.maxsize)
+
+
+    def add_connection(self, to_vertex, distance):
+        self.connections[to_vertex] = distance
+
+    def get_distance(self):
+        return self.distance
+
+    def __lt__(self, other):
+        return self.distance<other.distance
+
+
+    def __hash__(self):
+        return self.id
+
+
+
+class Graph(object):
+
+    def __init__(self):
+        self.vertexs = []
+        self.edges = []
+
+
+    def add_vertex(self, vertex):
+        if vertex in self.vertexs:
+            warnings.warn('%s is in graph'%(vertex))
+            return
+        self.vertexs.append(vertex)
+
+
+    def get_vertex(self, vertex):
+        if vertex in self.vertexs:
+            return vertex
+        return None
+
+
+    def add_edge(self,from_vertex, to_vertex, distance ):
+
+        if from_vertex not in self.vertexs:
+            self.add_vertex(from_vertex)
+
+        if to_vertex not in self.vertexs:
+            self.add_vertex(to_vertex)
+        from_vertex.add_connection(to_vertex, distance)
+
+    def __iter__(self):
+        return iter(self.vertexs)
+
+
+if __name__ == '__main__':
+    graph = Graph()
+    v_1 = Vertex(1)
+    v_2 = Vertex(2)
+    graph.add_edge(v_1, v_2, 10)
+    print(graph.vertexs[0].get_connections())
---
+++ shortedpath/shotpath.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#-*-coding=utf-8-*-
+
+import copy
+
+def dijkstra( graph, start):
+    path = []
+    vertex_list = copy.deepcopy(graph)
+    for v in vertex_list:
+        if v.id==start.id:
+            v.distance=0
+
+    vertex_list = sorted(vertex_list)
+
+    while vertex_list:
+        current_node = vertex_list.pop(0)
+        path.append(current_node)
+        for v,distance  in current_node.connections.items():
+            new_distance = current_node.get_distance() + distance
+            if new_distance < v.distance:
+                v.distance = new_distance
+                v.pre = current_node
+        vertex_list = sorted(vertex_list)
+
+
+    for v in path:
+        print (v, v.distance, v.pre)
+
+
+
+if __name__ == '__main__':
+    import graph
+    g = graph.Graph()
+    v_1 = graph.Vertex(1)
+    v_2 = graph.Vertex(2)
+    v_3 = graph.Vertex(3)
+
+    g.add_edge(v_1, v_2, 1)
+    g.add_edge(v_1, v_3, 3)
+    g.add_edge(v_2, v_3, 2)
+    dijkstra(g, v_1)
+
+
+
